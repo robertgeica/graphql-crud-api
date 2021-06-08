1951c35118000fcdce051a8145bf280fe3ac8cb8
Robert Geica <geicarobert@gmail.com>
2021-06-08 11:08:40 +0300

    tag mongoose model

---
+++ models/tag.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tagSchema = new Schema({
+  tagName: String,
+  tagColor: String,
+  noteId: String
+});
+
+module.exports = mongoose.model('Tag', tagSchema);
