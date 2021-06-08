8e008aa0cdbe7fce7b3c99046cfa8de811fc7589
Robert Geica <geicarobert@gmail.com>
2021-06-08 11:44:35 +0300

    add root query

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,6 @@ const Tag = require('../models/tag');
 
 const { GraphQLObjectType, GraphQLList, GraphQLString, GraphQLID } = graphql;
 
-// category type
 const CategoryType = new GraphQLObjectType({
   name: 'Category',
   fields: () => ({
@@ -20,7 +19,6 @@ const CategoryType = new GraphQLObjectType({
   }),
 });
 
-// note type
 const NoteType = new GraphQLObjectType({
   name: 'Note',
   fields: () => ({
@@ -40,11 +38,54 @@ const TagType = new GraphQLObjectType({
   name: 'Tag',
   fields: () => ({
     tagName: { type: GraphQLString },
-    tagColor: { type: GraphQLString }
-  })
-})
-// tag type
+    tagColor: { type: GraphQLString },
+  }),
+});
 
-// root query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    category: {
+      type: CategoryType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Category.findById(args.id);
+      },
+    },
+
+    note: {
+      type: NoteType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Note.findById(args.id);
+      },
+    },
+
+    categories: {
+      type: new GraphQLList(CategoryType),
+      resolve(parent, args) {
+        return Category.find({});
+      },
+    },
+
+    notes: {
+      type: new GraphQLList(NoteType),
+      resolve(parent, args) {
+        return Note.find({});
+      },
+    },
+
+    tags: {
+      type: new GraphQLList(TagType),
+      resolve(parent, args) {
+        return Tag.find({});
+      },
+    },
+  },
+});
 
 // mutations
+
+module.exports = new graphql.GraphQLSchema({
+  query: RootQuery,
+});
